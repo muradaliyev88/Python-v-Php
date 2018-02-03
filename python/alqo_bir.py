4c05357fdd28d26c4b0dd88c0a674b3995402701
Murad <33350189+muradaliyev88@users.noreply.github.com>
2018-02-03 13:31:37 +0400

    Add files via upload

---
+++ python/alqo_bir.py
@@ -0,0 +1,8 @@
+for d in range(5):
+    d +=1
+    #print("d nin qiymeti:",d)
+    for dd in range(5):
+        dd +=1
+        #print("dd nin qiymeti",dd)
+        print(d*dd,sep=',',end=' ')
+    print("\n")
